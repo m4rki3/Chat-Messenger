81dab1d1bf4798f3de55972692fc6ab60c5a83dd
Ilya <makovich.i@mail.ru>
2023-07-27 13:15:18 +0300

    IClient interface has been added. Some issues have been fixed. Server and client applications are functioning now.

---
+++ Chat/IClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient;
+public interface IClient : IDisposable
+{
+    public bool Connected { get; }
+    public void SendMessageToServer(string name, string message);
+    public string ReceiveChatLog();
+}
